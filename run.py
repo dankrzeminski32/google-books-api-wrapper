35edacb5f4a3d2d6be5c6dfc3b761c455a78c7a9
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-03-05 13:44:44 -0600

    update docs

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from google_books_api_wrapper.api import GoogleBooksAPI
 
 client = GoogleBooksAPI()
-response = client.search_book("atomic hdssadsadas")
+response = client.search_book("atomic habits")
 
-print(response.get_all_results())
+print(response.get_best_match().ISBN_10)
