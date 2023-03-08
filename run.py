d24e44401e55522f755e645279596b03e875c950
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-03-07 22:52:54 -0600

    isbn10

--- run.py
+++ run.py
@@ -3,4 +3,12 @@ from google_books_api_wrapper.api import GoogleBooksAPI
 client = GoogleBooksAPI()
 response = client.search_book("john")
 
-print(response.total_results)
+print(response.get_best_match().ISBN_13 + " " + response.get_best_match().title)
+print(response.get_best_match().ISBN_10 + " " + response.get_best_match().title)
+
+
+ten = client.get_book_by_isbn10(1610971027).ISBN_10
+turdteen = client.get_book_by_isbn13(9781610971027).ISBN_13
+
+print(ten)
+print(turdteen)
