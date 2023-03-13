3f7c8fa13ad8c0777b75584108314dea943f39eb
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-03-12 22:49:57 -0500

    feat: 7 unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from tests.utils.file_reader import read_json_file
-    
+   
 @pytest.fixture
 def google_books_multiple_books_response_data():
     payload = read_json_file('valid_get_book_response.json')
@@ -13,3 +13,5 @@ def google_books_no_books_in_response_data():
     "totalItems": 0
 }
     yield payload
+
+    
