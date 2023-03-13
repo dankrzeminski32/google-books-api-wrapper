90fc42750b48e46bb90ed842ac0b9922e8e7e697
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-03-12 22:56:56 -0500

    feat: 6 implement get_book_by_title and tests

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from google_books_api_wrapper.api import GoogleBooksAPI
 
 client = GoogleBooksAPI()
-response = client.get_books_by_author("stephen king")
+response = client.get_book_by_title("IT")
 
 print(response)
-
-for book in response:
-    print(book)
